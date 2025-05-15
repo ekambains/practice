8e06f460a68da5a2bc6c37ca4b8c69d444a11961
ekambains <bainseka@sheridancollege.ca>
2025-05-14 23:26:21 -0400

    Background 1

--- rauno/src/App.tsx
+++ rauno/src/App.tsx
@@ -1,10 +1,12 @@
-import FlashlightNav from "./components/FlashlightNav"
+import BgTiltedLines1 from "./components/BgTiltedLines1"
+
 
 function App() {
 
   return (
-    <div className="h-[100vh] w-full bg-[rgb(28,28,28)] flex justify-center items-center text-white">
-      <FlashlightNav />
+    <div className="h-[100vh] w-full flex justify-center items-center">
+      <BgTiltedLines1 />
+        <h1 className="text-black text-7xl font-bold">How's the Background?</h1>  
     </div>
   )
 }
---
+++ rauno/src/components/BgTiltedLines1.tsx
@@ -0,0 +1,9 @@
+
+
+const BgTiltedLines1 = () => {
+  return (
+    <div className="absolute inset-0 bg-[linear-gradient(45deg,#4f4f4f2e_50%,transparent_25%),linear-gradient(135deg,#4f4f4f2e_50%,transparent_25%)] bg-[size:20px_20px]"></div>
+  )
+}
+
+export default BgTiltedLines1
